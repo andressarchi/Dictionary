50a99156dfe1bf4dc1de0d585217a78f30571a6c
andressarchi <andresfelipesarchivalencia@gmail.com>
2024-12-02 13:01:24 -0500

    se hace cambios en el js y en el css

--- scripts/dictionaryApp.js
+++ scripts/dictionaryApp.js
@@ -1,21 +1,22 @@
 // Importar el objeto del diccionario desde el archivo dictionary.js
 import { dictionary } from './dictionary.js';
 
-// Obtener elementos del DOM
-const wordInput = document.getElementById('word'); // Campo de texto donde el usuario ingresa palabras
-const translateButton = document.getElementById('btn-traslate'); // Botón para traducir palabras
-const resultDiv = document.querySelector('.result p'); // Elemento donde se mostrará el resultado
-const categoryButtons = document.querySelectorAll('.list-word input[type="radio"]'); // Botones de selección de categorías
-const wordListContainer = document.querySelector('.container-list-word'); // Contenedor para listar palabras de categorías
-
-
 // Elementos del DOM
+const wordInput = document.getElementById('word'); 
+const translateButton = document.getElementById('btn-traslate'); 
+const resultDiv = document.querySelector('.result p'); 
+const categoryButtons = document.querySelectorAll('.list-word input[type="radio"]'); 
+const wordListContainer = document.querySelector('.container-list-word'); 
 const englishInput = document.getElementById('new-word-english');
 const spanishInput = document.getElementById('new-word-spanish');
 const categoryRadios = document.querySelectorAll('input[name="category"]');
 const addButton = document.getElementById('btn-add-word');
 const addResult = document.getElementById('add-result');
 const example= document.getElementById('new-word-example')
+
+
+
+
 // Función para agregar una palabra al diccionario
 function addWord() {
     const englishWord = englishInput.value.trim();
